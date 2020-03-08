17500dc70de23c4698b4176543ee811a1ffb137b
Josef <st.josef.b@gmail.com>
2020-03-08 20:00:14 +0700

    save annotation on referencing dataset

--- annotate/views.py
+++ annotate/views.py
@@ -13,23 +13,40 @@ def index(request):
     #message = ''
     #if 'message' in request.GET:
     #    message = request.GET['message']
+    is_ref_dataset = False
+    if 'refdataset' in request.GET:
+        is_ref_dataset = True
+        ref_dataset = request.GET['refdataset']
     dataset = request.GET['dataset']
     data_id = request.GET['data_id']
+
+    # display
     template = loader.get_template('annotate/index.html')
     # subfolders = [f.name for f in os.scandir(settings.BASE_DATASETS_PATH) if f.is_dir()]
     context = {
         'dataset': dataset,
-        'data_id': data_id
+        'data_id': data_id,
+        'is_ref_dataset': is_ref_dataset,
         # 'message': message
     }
+    if is_ref_dataset:
+        context['ref_dataset'] = ref_dataset
     return HttpResponse(template.render(context, request))
 
 
 def save(request):
     data_id = request.POST['data_id']
     dataset = request.POST['dataset']
+    is_ref_dataset = False
+    if 'refdataset' in request.POST:
+        is_ref_dataset = True
+        ref_dataset = request.POST['refdataset']
 
-    datasets_dir = '/'.join([settings.BASE_DATASETS_PATH, dataset, settings.DIR_DATASETS_ANNOTATIONS])
+    if is_ref_dataset:
+        datasets_dir = '/'.join([settings.BASE_DATASETS_PATH, dataset, settings.DIR_DATASETS_REFDATASETS,
+                                 ref_dataset, settings.DIR_DATASETS_ANNOTATIONS])
+    else:
+        datasets_dir = '/'.join([settings.BASE_DATASETS_PATH, dataset, settings.DIR_DATASETS_ANNOTATIONS])
 
     data = [[]]
     with open(datasets_dir + '/' + str(data_id) + '.json', 'w') as f:
